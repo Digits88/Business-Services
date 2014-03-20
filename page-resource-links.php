91f6f9d4b41f6ad3f878b85af4f3e78f01ff80d0
Rich Donohue <rgdonohue@gmail.com>
2014-03-20 11:37:02 -0500

    create page for custom fields of link resources

---
+++ page-resource-links.php
@@ -0,0 +1,64 @@
+<?php
+ 
+/**
+ * Template Name: Resource Links
+ */
+ 
+get_header(); 
+ 
+?>
+ 
+	<div id="main">
+
+		<div id="primary">
+		
+			<div id="content" role="main">
+ 
+		<?php while ( have_posts() ) : the_post(); ?>
+ 
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+			<p><?php the_content(); ?></p>
+			<?php 			
+			// check if the flexible content field has rows of data
+			if( have_rows('content_row') ):
+			 
+			     // loop through the rows of data
+			    while ( have_rows('content_row') ) : the_row();
+			 
+ 					?>
+ 					<h2><?php
+ 						 the_sub_field('sub_title') ?>
+ 					</h2><?php ;
+ 					?>
+ 					<div class="links"><?php
+ 					the_sub_field('resource_links'); ?>
+ 					</div><?php ;
+ 					?>
+ 					<div class="page-explanation"><?php
+ 						the_sub_field('resource_description'); ?>
+					</div><?php ;
+			    endwhile;
+			 
+			else :
+			 
+			    // no layouts found
+			 
+			endif;
+ 
+			?>
+
+ 
+		<?php endwhile; // end of the loop. ?>
+ 
+			</div><!-- #content -->
+			<?php get_sidebar(); ?>
+			<div class="clear"></div>
+			
+		</div><!-- #primary -->
+
+	</div>
+<?php get_footer(); ?>
+
+</div>
+
+get_header(); ?>
